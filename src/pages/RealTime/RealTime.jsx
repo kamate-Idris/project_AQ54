0011c3b4bcbc035ba0fc6d728b03f32f3dbc3ef8
Kamate-Idris <kat.dris13@gmail.com>
2024-01-12 16:00:45 +0000

    Ajust api call for every 5s

--- src/pages/RealTime/RealTime.jsx
+++ src/pages/RealTime/RealTime.jsx
@@ -26,10 +26,14 @@ const RealTime = () => {
         }
     };
 
+    // faire un fetch tout les 5s pour afficher les donnees en temps reel
     useEffect(() => {
-        fetchSensorDataAsync("SMART188");
-        fetchSensorDataAsync("SMART189");
-    }, []);
+        const interval = setInterval(() => {
+            fetchSensorDataAsync("SMART188");
+            fetchSensorDataAsync("SMART189");
+        }, 5000)
+        return () => clearInterval(interval)
+    }, [firstSensor, secondSensor]);
 
 
 
@@ -38,7 +42,7 @@ const RealTime = () => {
         firstSensor &&
         <div className="realtime">
             <div className="realtime-container">
-                <Table stationInfo={firstSensor}/>
+                <Table stationInfo={firstSensor} />
                 <Table stationInfo={secondSensor} />
             </div>
         </div>
